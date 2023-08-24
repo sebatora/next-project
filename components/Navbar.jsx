65f3a7e49e17b28729b1912bc9b8ae69b144e39d
Sebatora <sebastiana.toranzo@gmail.com>
2023-08-24 11:38:22 -0300

    apply styles tailwind install

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,7 +5,7 @@ import "./Navbar.css"
 function Navbar() {
   return (
     <nav className='navbar'>
-      <Link href="/"><h1>NextJS Project</h1></Link>
+      <Link href="/"><h1 className='text-5xl font-bold'>NextJS Project</h1></Link>
       <ul>
         <li><Link href="/">Home</Link></li>
         <li><Link href="/about">About</Link></li>
